f22ec19c8b2539223d75bbe83ac0c38f4dcb1202
MarsIdiot <1490784129@qq.com>
2019-07-17 22:51:11 +0800

    kafka-demofirst

---
+++ src/kafka/a_firstdemo/src/main/java/cn/Consumer.java
@@ -0,0 +1,45 @@
+package cn;
+
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.log4j.Logger;
+
+import java.util.Properties;
+
+public class Consumer {
+
+    static Logger log = Logger.getLogger(Producer.class);
+
+    private static final String TOPIC = "song";
+    private static final String BROKER_LIST = "xxx";
+    private static KafkaConsumer<String,String> consumer = null;
+
+    static {
+        Properties configs = initConfig();
+        consumer = new KafkaConsumer<String, String>(configs);
+    }
+
+    private static Properties initConfig(){
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers",BROKER_LIST);
+        properties.put("group.id","0");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("enable.auto.commit", "true");
+        properties.setProperty("auto.offset.reset", "earliest");
+        return properties;
+    }
+
+
+    public static void main(String[] args) {
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(10);
+            for (ConsumerRecord<String, String> record : records) {
+                log.info(record);
+            }
+        }
+    }
+}
