fd7c2024bbdb697842431db82985935f091a7898
tianlang.zhou <tianlang.zhou@ucarinc.com>
2019-07-25 15:02:40 +0800

    netty——服务端创建(加入bossgroup)

--- src/netty/g_netty_priavte_protocol/server/NettyServer.java
+++ src/netty/g_netty_priavte_protocol/server/NettyServer.java
@@ -3,10 +3,13 @@ package netty.g_netty_priavte_protocol.server;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import netty.g_netty_priavte_protocol.NettyConstant;
 import netty.g_netty_priavte_protocol.codecMarshalling.NettyMessageDecoder;
@@ -18,11 +21,14 @@ import netty.g_netty_priavte_protocol.codecMarshalling.NettyMessageEncoder;
 public class NettyServer {
 
     public void start(String host,int port) throws Exception {
-        EventLoopGroup group = new NioEventLoopGroup();
+        EventLoopGroup bossgroup = new NioEventLoopGroup();
+        EventLoopGroup workergroup = new NioEventLoopGroup();
         try {
             ServerBootstrap sb = new ServerBootstrap();
-            sb.group(group) // 绑定线程池
+            sb.group(bossgroup,workergroup) // 绑定线程池
                     .channel(NioServerSocketChannel.class) // 指定使用的channel  服务端
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
                     .localAddress(host,port)// 绑定服务端ip和端口
                     .childHandler(new ChannelInitializer<SocketChannel>() { // 绑定客户端连接时候触发操作
 
@@ -54,7 +60,8 @@ public class NettyServer {
             System.out.println(NettyServer.class + " started and listen on " + cf.channel().localAddress());
             cf.channel().closeFuture().sync(); // 关闭服务器通道
         } finally {
-            group.shutdownGracefully().sync(); // 释放线程池资源
+            bossgroup.shutdownGracefully().sync(); // 释放线程池资源
+            workergroup.shutdownGracefully().sync(); // 释放线程池资源
         }
     }
 
